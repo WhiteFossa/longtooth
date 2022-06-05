688912d2f52439c905250c5e126b0fd44b0c26f4
White Fossa <whitefossa@gmail.com>
2022-06-05 10:43:25 +0200

    Desktop app skeleton

---
+++ longtooth/longtooth.Desktop/Program.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace longtooth.Desktop
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Dependency injection service provider
+        /// </summary>
+        public static ServiceProvider Di { get; set; }
+
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            // Preparing DI
+            Di = ConfigureServices()
+                .BuildServiceProvider();
+
+            BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+        }
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace();
+
+        // Setting up DI
+        public static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            return services;
+        }
+    }
+}
