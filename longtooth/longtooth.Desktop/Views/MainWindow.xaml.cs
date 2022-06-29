ba92e9bfc9862fd7caf178ccbb1285bb931f43e9
Artem Vetrov <a.vetrov@bankingon.io>
2022-06-29 09:28:13 +0200

    Removed a lot of experimental stuff, preparing for Dokan support

--- longtooth/longtooth.Desktop/Views/MainWindow.xaml.cs
+++ longtooth/longtooth.Desktop/Views/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using longtooth.Common.Abstractions.DTOs;
-using longtooth.Desktop.ViewModels;
 
 namespace longtooth.Desktop.Views
 {
@@ -15,36 +13,6 @@ namespace longtooth.Desktop.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        private async void OnMountpointCellPressedAsync(object sender, DataGridCellPointerPressedEventArgs e)
-        {
-            var pressedItem = e.Cell.DataContext as MountpointDto;
-
-            if (pressedItem == null)
-            {
-                return;
-            }
-
-            var serverSidePath = pressedItem.ServerSidePath;
-
-            var viewModel = (sender as Control).DataContext as MainWindowViewModel;
-
-            await viewModel.OnMountpointChangedAsync(serverSidePath);
-        }
-
-        private async void OnDirectoryContentCellPressedAsync(object sender, DataGridCellPointerPressedEventArgs e)
-        {
-            var pressedItem = e.Cell.DataContext as DirectoryContentItemDto;
-
-            if (pressedItem == null)
-            {
-                return;
-            }
-
-            var viewModel = (sender as Control).DataContext as MainWindowViewModel;
-
-            await viewModel.OnDirectoryContentCellChangedAsync(pressedItem);
         }
     }
 }
